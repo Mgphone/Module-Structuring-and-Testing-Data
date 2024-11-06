03d7b08c2dde89cbb26fbc16a959ab6f7b633052
mgphone <pmn.blazer@gmail.com>
2024-11-06 19:29:55 +0000

    fix errors

--- Sprint-1/errors/0.js
+++ Sprint-1/errors/0.js
@@ -1,2 +1,6 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+console.log(
+  "This is just an instruction for the first activity - but it is just for human consumption"
+);
+console.log(
+  "We don't want the computer to run these 2 lines - how can we solve this problem?"
+);
--- Sprint-1/errors/4.js
+++ Sprint-1/errors/4.js
@@ -1,2 +1,5 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+// const 12HourClockTime = "20:53";
+// const 24hourClockTime = "08:53";
+// variable can not start with number
+const hourClockTime12 = "20:53";
+const hourClockTime24 = "08:53";
