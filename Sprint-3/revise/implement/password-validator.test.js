a4edfb1a567b5986ed31652bc046ad9a139041d7
mgphone <pmn.blazer@gmail.com>
2024-11-21 01:32:17 +0000

    test for revise implement done in jest

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,60 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+//using outside to check lower alphabet
+const isLowerCase = (newPassword) => {
+  const lowerCaseLetter = "abcdefghijklmnopqrstuvwxyz";
+  const makeArrayPassword = newPassword.split("");
+  for (i = 0; i < makeArrayPassword.length; i++) {
+    if (lowerCaseLetter.includes(makeArrayPassword[i])) {
+      return true;
+    }
+    return false;
+  }
+};
+const checkPasswordValidation = (newPassword) => {
+  //this is for 5 characters
+  //   if (newPassword.length < 5) {
+  //     return "Character should be more than 5";
+  //   }
+  const isCharAtLeaseFive = newPassword.length >= 5;
+  //   return newPassword;
+  //this is for upperCase Regex
+  const upperCaseRegex = /[A-Z]/g;
+  const isCharContainOneCapital = !!newPassword.match(upperCaseRegex);
+  //   return !!newPassword.match(upperCaseRegex);
+  //this is lowerCase
+  //   return isLowerCase(newPassword);
+  const isCharContainOneLowerLetter = isLowerCase(newPassword);
+  //this is for zero to 9
+  const digitRegex = /[0-9]/g;
+  const isCharContainOneNumber = !!newPassword.match(digitRegex);
+  //   return isCharContainOneNumber;
+  //   return !!newPassword.match(digitRegex);
+  //this is for special character "!", "#", "$", "%", ".", "*", "&"
+  const specialRegex = /[!#$%.*&]/g;
+  const isCharContainOneSpecialLetter = !!newPassword.match(specialRegex);
+  //   return !!newPassword.match(specialRegex);
+  //old password
+  //   return isCharContainOneSpecialLetter;
+  const previousPassword = ["mg-Phone", "Phone."]; //this might coming from somewhere from database
+  //   return previousPassword.includes(newPassword);
+  const isPasswordMatchOld = previousPassword.includes(newPassword);
+  //   return !isPasswordMatchOld;
+  return isCharAtLeaseFive &&
+    isCharContainOneCapital &&
+    isCharContainOneLowerLetter &&
+    isCharContainOneNumber &&
+    isCharContainOneSpecialLetter &&
+    !isPasswordMatchOld
+    ? "Password Valid"
+    : "Password Invalid";
+};
+// console.log(checkPasswordValidation("ancd0A."));
+test("Checking Invalid", () => {
+  expect(checkPasswordValidation("abcd")).toBe("Password Invalid");
+});
+
+test("Checking Valid", () => {
+  expect(checkPasswordValidation("ancd0A.")).toBe("Password Valid");
+});
--- Sprint-3/revise/implement/repeat.test.js
+++ Sprint-3/revise/implement/repeat.test.js
@@ -23,3 +23,41 @@
 // Given a target string str and a negative integer count,
 // When the repeat function is called with these inputs,
 // Then it should throw an error or return an appropriate error message, as negative counts are not valid.
+const repeatString = (str, count) => {
+  if (typeof str !== "string" || typeof count !== "number") {
+    return "Make Sure Parameter have to be String or Number";
+  }
+  if (count < 0) {
+    throw new Error("Negative counts are not valid");
+  }
+  let outputResult = "";
+  for (let i = 0; i < count; i++) {
+    outputResult += str;
+  }
+  return outputResult;
+};
+// console.log(repeatString(true, 2));
+test("Check valid string", () => {
+  expect(repeatString(2, 1)).toBe(
+    "Make Sure Parameter have to be String or Number"
+  );
+});
+test("Check valid count", () => {
+  expect(repeatString(2, true)).toBe(
+    "Make Sure Parameter have to be String or Number"
+  );
+});
+//to throw have to be empty or anonymous function
+test("Check count less than zero", () => {
+  //   expect(repeatString("Hello", -1)).toThrow("Negative counts are not valid");
+  expect(() => repeatString("Hello", -1)).toThrow(
+    "Negative counts are not valid"
+  );
+});
+
+test("Check count is zero", () => {
+  expect(repeatString("Hello", 0)).toBe("");
+});
+test("Check count is more than one positive Integer", () => {
+  expect(repeatString("Hello", 3)).toBe("HelloHelloHello");
+});
